857cfbcf3408e8c3bded5b736d1669a9010ba422
ousttrue <ousttrue@gmail.com>
2016-09-11 15:33:00 +0900

    02-enumerate_devices

--- source/app.d
+++ source/app.d
@@ -3,11 +3,13 @@ import derelict.glfw3.glfw3;
 import derelict.glfw3.glfw3;
 import dvulkan;
 import std.experimental.logger;
+import std.exception;
 
 
 struct VulkanManager
 {
     VkInstance inst;
+    VkPhysicalDevice[] gpus;
 
 	static this()
 	{
@@ -24,34 +26,49 @@ struct VulkanManager
 
     bool initialize()
     {
-        // initialize the VkApplicationInfo structure
-        VkApplicationInfo app_info = {
-			pApplicationName: "VulkanOnD",
-			apiVersion: VK_MAKE_VERSION(1, 0, 2),
-		};
+        {
+            // 01
 
-        // initialize the VkInstanceCreateInfo structure
-        VkInstanceCreateInfo inst_info = {
-			pApplicationInfo: &app_info,
-		};
+            // initialize the VkApplicationInfo structure
+            VkApplicationInfo app_info = {
+                pApplicationName: "VulkanOnD",
+                apiVersion: VK_MAKE_VERSION(1, 0, 2),
+            };
 
-        auto res = vkCreateInstance(&inst_info, null, &inst);
-        if (res != VkResult.VK_SUCCESS){
-			if(res == VkResult.VK_ERROR_INCOMPATIBLE_DRIVER) {
-				error("cannot find a compatible Vulkan ICD");
-				return false;
-			} 
-			else  {
-				error("unknown error");
-				return false;
-			}
-		}
+            // initialize the VkInstanceCreateInfo structure
+            VkInstanceCreateInfo inst_info = {
+                pApplicationInfo: &app_info,
+            };
 
-		loadInstanceFunctions(inst);
-		log(vkDestroyInstance);
+            auto res = vkCreateInstance(&inst_info, null, &inst);
+            if (res != VkResult.VK_SUCCESS){
+                if(res == VkResult.VK_ERROR_INCOMPATIBLE_DRIVER) {
+                    error("cannot find a compatible Vulkan ICD");
+                    return false;
+                } 
+                else  {
+                    error("unknown error");
+                    return false;
+                }
+            }
+
+            loadInstanceFunctions(inst);
+            enforce(vkDestroyInstance, "loadInstanceFunctions");
+        }
+
+        {
+            // 02
+            uint gpu_count = 1;
+            auto res =
+                vkEnumeratePhysicalDevices(inst, &gpu_count, null);
+            enforce(gpu_count, "gpu_count");
+
+            gpus=new VkPhysicalDevice[gpu_count];
+            res = vkEnumeratePhysicalDevices(inst, &gpu_count, gpus.ptr);
+            enforce(!res && gpu_count >= 1, "vkEnumeratePhysicalDevices");
+        }
 
         info("vkCreateInstance success");
-
         return true;
     }
 }
@@ -79,7 +96,7 @@ struct Glfw3Manager
 		const window_width  = 800;
 		const window_height = 600;
 		window = glfwCreateWindow(window_width, window_height
-											  , "GLFW 3 D-lang"
+											  , "VulkanOnD"
 											  , null, null);
 		log("create window.");
 
